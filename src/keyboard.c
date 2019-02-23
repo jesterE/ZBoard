4717be37e5cf505fd0163298329b3a07f417da81
Eli Jester <jestereli@gmail.com>
2019-02-23 16:59:45 -0600

    Add code to disable watchdog timer

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,5 +1,15 @@
 #include "descriptors.h"
 
+#include <avr/wdt.h>
+
+// The watchdog isn't used, but is still disabled in case it gets enabled accidentily,
+// as recommended in the datasheet
+void kill_watchdog(void) __attribute__((naked)) __attribute__((section (".init3")));
+void kill_watchdog(void) {
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
+}
+
 int main(void) {
     while (1) {
         __asm__ __volatile__ ("nop" ::);
